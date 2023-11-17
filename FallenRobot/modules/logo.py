c674b3335618e1cf6f028ad5e9402610d4987805
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-11-17 12:09:48 +0530

    Update logo.py

--- FallenRobot/modules/logo.py
+++ FallenRobot/modules/logo.py
@@ -319,7 +319,7 @@ async def lego(event):
             os.remove(fname)
 
 
-__mod_name__ = "Lᴏɢᴏ"
+__mod_name__ = "Logo"
 
 __help__ = """
 I can create some beautiful and attractive logo for your profile pics.
