ccda9717fa9cee87247772b45bbd6b980214301c
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-11-17 12:23:19 +0530

    Update tagall.py

--- FallenRobot/modules/tagall.py
+++ FallenRobot/modules/tagall.py
@@ -97,7 +97,7 @@ async def cancel_spam(event):
         return await event.respond("__Stopped mention.__")
 
 
-__mod_name__ = "Tᴀɢ Aʟʟ"
+__mod_name__ = "Mention"
 __help__ = """
 *Only for admins*
 
