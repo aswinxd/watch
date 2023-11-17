92276a3cc40841ef5097a5ea0659aed1150a2fb9
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-11-17 11:57:44 +0530

    Update cust_filters.py

--- FallenRobot/modules/cust_filters.py
+++ FallenRobot/modules/cust_filters.py
@@ -640,7 +640,7 @@ Check ❍ /markdownhelp to know more!
 
 """
 
-__mod_name__ = "Fɪʟᴛᴇʀs"
+__mod_name__ = "Filters"
 
 FILTER_HANDLER = CommandHandler("filter", filters, run_async=True)
 STOP_HANDLER = CommandHandler("stop", stop_filter, run_async=True)
