b4b5e4c93a4a298f67834a737515e1415d90a828
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-06 15:37:03 +0530

    Update misc.py

--- FallenRobot/modules/misc.py
+++ FallenRobot/modules/misc.py
@@ -81,7 +81,13 @@ __help__ = """
 *Available commands:*
 *Markdown:*
  ❍ /markdownhelp*:* quick summary of how markdown works in telegram - can only be called in private chats
- 
+
+ Available Commands For Group :
+ ❍ /setgtitle <newtitle>: Sets new chat title in your group.
+ ❍ /setgpic: As a reply to file or photo to set group profile pic!
+ ❍ /delgpic: Same as above but to remove group profile pic.
+ ❍ /setsticker: As a reply to some sticker to set it as group sticker set!
+ ❍ /setdescription <description>: Sets new chat description in group.
 *Other commands on misc will be relesed on our next update*
 > checkout @AmiciaRobot for your group voice chat
 
