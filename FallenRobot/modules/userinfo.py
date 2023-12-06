5497f7d88ce817f8d39e8f128079718df20d6ef1
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-06 15:32:05 +0530

    Update userinfo.py

--- FallenRobot/modules/userinfo.py
+++ FallenRobot/modules/userinfo.py
@@ -501,31 +501,6 @@ def __user_info__(user_id):
     result = result.strip("\n")
     return result
 
-
-__help__ = """
-*ID:*
-  /id*:* get the current group id. If used by replying to a message, gets that user's id.
-  /gifid*:* reply to a gif to me to tell you its file ID.
-
-*Self added information:* 
-  /setme <text>*:* will set your info
-  /me*:* will get your or another user's info.
-*Examples:* 
-  /setme I am a wolf.
-  /me @username(defaults to yours if no user specified)
-
-*Information others add on you:* 
-  /bio*:* will get your or another user's bio. This cannot be set by yourself.
-  /setbio <text>*:* while replying, will save another user's bio 
-*Examples:* 
-  /bio @username(defaults to yours if not specified).`
-  /setbio This user is a wolf` (reply to the user)
-
-*Overall Information about you:*
-  /info*:* get information about a user. 
-  /myinfo*:* Shows info about the user who sent this command.
-"""
-
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
 GET_BIO_HANDLER = DisableAbleCommandHandler("bio", about_bio)
 
