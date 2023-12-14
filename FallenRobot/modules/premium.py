c5fa7462828539b3ee80770bba6ed5d42a90781b
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-14 23:47:39 +0530

    Update premium.py

--- FallenRobot/modules/premium.py
+++ FallenRobot/modules/premium.py
@@ -1,8 +1,21 @@
-help += """
-*Callback Buttons:*
-1. /newbutton1 - Description for new button 1
-2. /newbutton2 - Description for new button 2
-3. /newbutton3 - Description for new button 3
-4. /newbutton4 - Description for new button 4
-5. /newbutton5 - Description for new button 5
+from telethon.tl.custom import Button
+
+__help__ = """
+*Admins Only*
+
+ /nightmode*:* Adds Group to NightMode Chats
+ /rmnight*:* Removes Group From NightMode Chats
+
+*Note:* Night Mode chats get Automatically closed at 12 am(IST) and Automatically opened at 6 am(IST) to Prevent Night Spam.
+
+*Additional Options:*
+ - `/nightmodehelp`: Show additional commands for Night Mode settings
 """
+
+# Add callback buttons directly in __help__
+__help__ += "\n*Additional Options:*"
+__help__ += "\n - `/nightmodehelp`: Show additional commands for Night Mode settings"
+__help__ += f"\n - {Button.callback('Enable Night Mode', data='enable_night_mode')}"
+__help__ += f"\n - {Button.callback('Disable Night Mode', data='disable_night_mode')}"
+
+__mod_name__ = "Night mode"
