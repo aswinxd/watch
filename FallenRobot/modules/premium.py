a8b9ffa91f32a106383b3ec39796c395393fa70b
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-14 20:25:39 +0530

    Update premium.py

--- FallenRobot/modules/premium.py
+++ FallenRobot/modules/premium.py
@@ -1,8 +1,8 @@
-__help__ = """
- 
- *oya*
-*this is premium functions on the bot*
-
+help += """
+*Callback Buttons:*
+1. /newbutton1 - Description for new button 1
+2. /newbutton2 - Description for new button 2
+3. /newbutton3 - Description for new button 3
+4. /newbutton4 - Description for new button 4
+5. /newbutton5 - Description for new button 5
 """
-
-__mod_name__ = "premium"
