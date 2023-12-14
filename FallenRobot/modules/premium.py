5b5aa417da68908e771c69f71d25b60c4f144a68
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-15 00:00:38 +0530

    Update premium.py

--- FallenRobot/modules/premium.py
+++ FallenRobot/modules/premium.py
@@ -15,7 +15,7 @@ __help__ = """
 # Add callback buttons directly in __help__
 __help__ += "\n*Additional Options:*"
 __help__ += "\n - `/nightmodehelp`: Show additional commands for Night Mode settings"
-__help__ += f"\n - {Button.callback('Enable Night Mode', data='enable_night_mode')}"
-__help__ += f"\n - {Button.callback('Disable Night Mode', data='disable_night_mode')}"
+__help__ += f"\n - {Button.inline('Enable Night Mode', data='enable_night_mode')}"
+__help__ += f"\n - {Button.inline('Disable Night Mode', data='disable_night_mode')}"
 
 __mod_name__ = "Night mode"
