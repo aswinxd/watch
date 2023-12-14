a28a0aaf9529bbd0e578076c4834bd7ccfa1f262
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-12-15 00:39:08 +0530

    Update premium.py

--- FallenRobot/modules/premium.py
+++ FallenRobot/modules/premium.py
@@ -10,10 +10,10 @@ __help__ = """
  - `/nightmodehelp`: Show additional commands for Night Mode settings
 
 *Additional URL Options:*
- - Enable Night Mode: <a href='http://yourwebsite.com/enablenightmode'>Enable Night Mode</a>
- - Disable Night Mode: <a href='http://yourwebsite.com/disablenightmode'>Disable Night Mode</a>
- - Visit our Website: <a href='http://yourwebsite.com'>Visit our Website</a>
- - GitHub Repository: <a href='https://github.com/your_username/your_repo'>GitHub Repository</a>
+ - [Enable Night Mode](http://yourwebsite.com/enablenightmode)
+ - [Disable Night Mode](http://yourwebsite.com/disablenightmode)
+ - [Visit our Website](http://yourwebsite.com)
+ - [GitHub Repository](https://github.com/your_username/your_repo)
 """
 
 __mod_name__ = "Night mode"
