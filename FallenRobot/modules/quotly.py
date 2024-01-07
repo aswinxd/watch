a814d1923a1765f5b5ed61c1a1ce265ee6628161
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-07 14:25:48 +0530

    Update quotly.py

--- FallenRobot/modules/quotly.py
+++ FallenRobot/modules/quotly.py
@@ -9,7 +9,7 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import functions, types
 
-from DewmiBot.events import register
+from FallenRobot.events import register
 
 COLORS = [
     "#F07975",
