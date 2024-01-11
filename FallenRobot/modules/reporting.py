c2709933a3ae90aa044dad8888f92f120683755e
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-11 15:14:47 +0530

    Update reporting.py

--- FallenRobot/modules/reporting.py
+++ FallenRobot/modules/reporting.py
@@ -261,13 +261,13 @@ def buttons(update: Update, context: CallbackContext):
 
 
 __help__ = """
- ❍ /report <reason>*:* reply to a message to report it to admins.
- ❍ @admin*:* reply to a message to report it to admins.
+  /report <reason>*:* reply to a message to report it to admins.
+  @admin*:* reply to a message to report it to admins.
  
 *Note :* Neither of these will get triggered if used by admins.
 
 *Admins only:*
- ❍ /reports <on/off>*:* change report setting, or view current status.
+  /reports <on/off>*:* change report setting, or view current status.
    • If done in pm, toggles your status.
    • If in group, toggles that groups's status.
 """
