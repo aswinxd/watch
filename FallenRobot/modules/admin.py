823ca90cc6eb193434d080ef979a1b0c599eab40
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-11 23:30:26 +0530

    Update admin.py

--- FallenRobot/modules/admin.py
+++ FallenRobot/modules/admin.py
@@ -884,25 +884,25 @@ def adminlist(update, context):
 
 __help__ = """
 *User Commands*:
-/admins*:* list of admins in the chat
-/pinned*:* to get the current pinned message.
+-/admins*:* list of admins in the chat
+-/pinned*:* to get the current pinned message.
 
 *The Following Commands are Admins only:* 
-/pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
-/unpin*:* unpins the currently pinned message
-/invitelink*:* gets invitelink
-/promote*:* promotes the user replied to
-/lowpromote*:* promotes the user replied to with half rights
-/fullpromote*:* promotes the user replied to with full rights
-/demote*:* demotes the user replied to
-/admincache*:* force refresh the admins list
+-/pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+-/unpin*:* unpins the currently pinned message
+-/invitelink*:* gets invitelink
+-/promote*:* promotes the user replied to
+-/lowpromote*:* promotes the user replied to with half rights
+-/fullpromote*:* promotes the user replied to with full rights
+-/demote*:* demotes the user replied to
+-/admincache*:* force refresh the admins list
 
 *Purge*
-/del*:* deletes the message you replied to
-/purge*:* deletes all messages between this and the replied to message.
-/purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+-/del*:* deletes the message you replied to
+-/purge*:* deletes all messages between this and the replied to message.
+-/purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
 
-userinfo: use /info 
+-/info: to know the info of user on db
 """
 
 SET_DESC_HANDLER = CommandHandler("setdesc", set_desc, run_async=True)
